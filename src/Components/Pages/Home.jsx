f6af1d8316744db763da15974674b9940d2cf526
BartoszCoJest <bartoszcojest@gmail.com>
2024-04-02 18:29:02 +0200

    Troche ui i zapisywanie jako PDF

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,41 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import mammoth from "mammoth";
 import parse from "html-react-parser";
-//import PizZip from 'pizzip';
-//import { saveAs } from 'file-saver';
-//import { PDFDocument } from 'pdf-lib';
-//import { PDFViewer, Document, Page, Text, PDFDownloadLink  } from '@react-pdf/renderer';
-//import HTMLtoDOCX from "html-to-docx";
+import html2pdf from "html2pdf.js";
+import bgImage from "../../assets/bg.jpg";
+import "./Home.css";
 
 function Home() {
   const [docxContent, setDocxContent] = useState(null);
-  const [plainText, setPlainText] = useState(null);
   const [keywords, setKeywords] = useState([]);
+  const customTextRefs = useRef([]);
 
   const onFileUpload = (event) => {
-    console.log("File selected");
     const reader = new FileReader();
     let file = event.target.files[0];
 
     reader.onload = (e) => {
-      console.log("File loaded");
       const content = e.target.result;
-      mammoth.convertToHtml({ arrayBuffer: content })
+      mammoth
+        .convertToHtml({ arrayBuffer: content })
         .then((result) => {
           setDocxContent(result.value);
-        })
-        .catch((err) => {
-          console.error("Error converting DOCX to HTML:", err); // zabezpieczenie
-        });
-
-      mammoth.extractRawText({ arrayBuffer: content })
-        .then((result) => {
-          
-          setPlainText(result.value);
           findKeywords(result.value);
         })
         .catch((err) => {
-          console.error("Error extracting plain text from DOCX:", err);
+          console.error("Error converting DOCX to HTML:", err); // Zabezpieczenie
         });
     };
 
@@ -43,125 +31,124 @@ function Home() {
 
     reader.readAsArrayBuffer(file);
   };
-
   // Funkcja znajdująca słowa
   const findKeywords = (html) => {
     const extractedKeywords = [];
-    const doc = new DOMParser().parseFromString(html, 'text/html');
-    const elements = doc.querySelectorAll('*');
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    const elements = doc.querySelectorAll("*");
 
     elements.forEach((element) => {
-        const text = element.textContent;
-        const regex = /(.+?)\s*\.\.\./g;
-        let match;
-        while ((match = regex.exec(text)) !== null) {
-            const keyword = match[1].trim();
-            if (keyword !== "" && !extractedKeywords.some(item => item.word === keyword)) {
-                extractedKeywords.push({ word: keyword, customText: "" });
-            }
+      const text = element.textContent;
+      const regex = /(.+?)\s*\.\.\./g;
+      let match;
+      while ((match = regex.exec(text)) !== null) {
+        const keyword = match[1].trim();
+        if (
+          keyword !== "" &&
+          !extractedKeywords.some((item) => item.word === keyword)
+        ) {
+          extractedKeywords.push({ word: keyword, customText: "" });
         }
+      }
     });
 
-    console.log("Wyrazy przed znacznikiem '...':", extractedKeywords);
+    console.log("Wyrazy przed znacznikiem '...':", extractedKeywords); // Wyświetlenie wyrazów przed "..." w konsoli
     setKeywords(extractedKeywords);
-};
+  };
 
-
-
-
-
-
-
-  // Funkcja obsługująca zmianę niestandardowego tekstu
   const handleCustomTextChanged = (index, event) => {
     const updatedKeywords = [...keywords];
     updatedKeywords[index].customText = event.target.value;
     setKeywords(updatedKeywords);
   };
-  console.log("Content of docxContent:", keywords); 
-  
-
-
-
-  const saveChanges = async () => {
-    const keywordCustomTextMap = {};
-    keywords.forEach(keyword => {
-        keywordCustomTextMap[keyword.word] = keyword.customText;
+  // Pobieramy zaktualizowany kod HTML
+  const saveChanges = () => {
+    const updatedHtmlContent = applyCustomTextChanges(docxContent);
+    generatePdf(updatedHtmlContent);
+  };
+  // Funkcja obsługująca zmianę niestandardowego tekstu
+  const applyCustomTextChanges = (htmlContent) => {
+    keywords.forEach((keyword, index) => {
+      const regex = new RegExp(`${keyword.word}\\s*\\.{3}`, "g");
+      htmlContent = htmlContent.replace(regex, keyword.customText);
     });
+    return htmlContent;
+  };
 
-    const doc = new DOMParser().parseFromString(docxContent, 'text/html');
-    console.log(doc);
+  const generatePdf = (htmlContent) => {
+    const options = {
+      margin: 10,
+      filename: "document.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
 
-    // Przetwarzamy cały tekst w dokumencie
-    const textNodes = doc.createTreeWalker(doc.body, NodeFilter.SHOW_TEXT, null, false);
-    let currentNode;
-    while (currentNode = textNodes.nextNode()) {
-      console.log("sprawdz1")
-        Object.keys(keywordCustomTextMap).forEach(keyword => {
-            //tutaj wybor sekwencji
-         //  const regex = new RegExp(`?<=${keyword}\\s*\\.{3}`, 'gi');
-            const regex = new RegExp(`(?<=\\b${keyword}\\s*)\\.{3}`, 'g');
-
-            // Sprawdzamy, czy aktualny węzeł tekstowy zawiera sekwencję "..." po słowie kluczowym
-            if (regex.test(currentNode.nodeValue)) {
-                // Zamieniamy sekwencję "..." na niestandardowy tekst dla danego słowa kluczowego
-                console.log("sprawdz2");
-                currentNode.nodeValue = currentNode.nodeValue.replace(regex, keywordCustomTextMap[keyword]);
-            }
-        });
-    }
-
+    const customTexts = keywords.map(
+      (keyword, index) => customTextRefs.current[index].value
+    );
     // Pobieramy zaktualizowany kod HTML
-    const updatedHtmlContent = doc.documentElement.outerHTML;
-    console.log("Zaktualizowany dokument HTML:", updatedHtmlContent);
-   // setPdfDocument(updatedHtmlContent);
-   //const content = updatedHtmlContent;
+    const htmlWithCustomText = applyCustomTextToHTML(htmlContent, customTexts);
 
-//const data = await HTMLtoDOCX(content);
-   // saveAs(data, "hello.docx");
+    html2pdf().from(htmlWithCustomText).set(options).save();
+  };
 
-
-};
-
-
-
-
-
-
-
-
-
-
-  
-
+  const applyCustomTextToHTML = (htmlContent, customTexts) => {
+    keywords.forEach((keyword, index) => {
+      const regex = new RegExp(`${keyword.word}\\s*\\.{3}`, "g");
+      htmlContent = htmlContent.replace(regex, customTexts[index]);
+    });
+    return htmlContent;
+  };
 
   return (
     <div>
-      <input type="file" onChange={onFileUpload} name="docx-reader" />
-      {docxContent && (
-        <div>
-          <h2>Wyświetlanie dokumentu DOCX:</h2> 
-          <div>{parse(docxContent)}</div>
-        </div>
-      )}
-      {keywords.length > 0 && (
-        <div>
-          <h2>Wyrazy przed znacznikiem "..." :</h2>
-          <div>
-            {keywords.map((keyword, index) => (
-              <div key={index}>
-                {keyword.word}:{" "}
-                <input
-                  type="text"
-                  value={keyword.customText}
-                  onChange={(event) => handleCustomTextChanged(index, event)}
-                />
+      <img src={bgImage} alt="es" className="bg-img" />
+      <div className="wszystko1">
+        <input
+          type="file"
+          onChange={onFileUpload}
+          name="docx-reader"
+          className="inpu"
+        />
+        <div className="sw">
+          {" "}
+          {docxContent && (
+            <div>
+              <h2>
+                <span className="koks">Wyświetlanie dokumentu DOCX:</span>
+              </h2>
+              <div className="cont">{parse(docxContent)}</div>
+            </div>
+          )}
+          {keywords.length > 0 && (
+            <div>
+              <h2>
+                <span className="koks">Wyrazy przed znacznikiem "..." :</span>
+              </h2>
+              <div className="znacz">
+                {keywords.map((keyword, index) => (
+                  <div key={index}>
+                    {keyword.word}:{" "}
+                    <input
+                      className="in"
+                      type="text"
+                      ref={(ref) => (customTextRefs.current[index] = ref)}
+                      defaultValue={keyword.customText}
+                      onChange={(event) =>
+                        handleCustomTextChanged(index, event)
+                      }
+                    />
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-          <button onClick={saveChanges}>Zapisz</button>
+              <button onClick={saveChanges} className="btnzap">
+                Zapisz jako pdf
+              </button>
+            </div>
+          )}{" "}
         </div>
-      )}
+      </div>
     </div>
   );
 }
